f55415b85aa096954c1864ecee0564a4a1520410
kneerunjun <kneerunjun@gmail.com>
2018-04-26 14:45:15 +0530

    +code for redesign the crons

--- aqsm.device/schedules.py
+++ aqsm.device/schedules.py
@@ -3,8 +3,8 @@
 #   development         :24-APRIL-2018
 #   purpose             :This helps setting up the cron jobs for the tank assiting tasks
 from apscheduler.schedulers.background import BackgroundScheduler
-import json, sys, datetime, pdb, hardware
-# initially we would want to pick from settings the cron job timing
+import json, sys, datetime, pdb,
+
 try:
     with open('./settings.json') as data_file:
         settings = json.load(data_file)
@@ -12,87 +12,50 @@ try:
 except FileNotFoundError as fe:
     print("schedules.py : failed to open the settings.json file ,check if the file is present and valid")
     sys.exit(1)
-
-# now once we have the settings , we need to go ahead and set initial state of the devices according to the time we have now. Else lets say we start the program at 18:01 , the jobs devices that should have been turned ON at 18:00 would not have started anyway
-# NOTE: Setting the initial state for each of the devices at the start of the program
-# everything here is based on finding the settings.json file
 if settings!=None:
     sched = BackgroundScheduler()
+# all these are slots in the schedules of an entire day
+# these are basically the slots where all you can go ahead and change the relay states
+riseandshine = settings["crons"]["riseandshine"]
+middaycalm = settings["crons"]["middaycalm"]
+lateafternoon = settings["crons"]["lateafternoon"]
+twilight = settings["crons"]["twilight"]
+supper = settings["crons"]["supper"]
+night = settings["crons"]["night"]
+midnight = settings["crons"]["midnight"]
+darknight = settings["crons"]["darknight"]
 
-# def compress_list_tostring(lst):
-#     '''This helps to convert the incoming list from the json to a comma separated string
-#     Comma separated string is used to setup the cron job on specific hours
-#     '''
-#     return ','.join(map(str, lst))
-# def device_schedule(devicename, on_off=1):
-#     '''This digs into the settins dictionary and gets the ON, OFF schedule as required
-#     returns a tuple (hours , minutes) that signify the hours & minutes at which the triggers are fired. It spits out an list , so you may have to compress_list_tostring before setting up cron jobs
-#     on_off      : schedule for on time ==1 and schedule for off time ==0
-#     '''
-#     if settings!=None:
-#         device =settings["crons"]["{0}".format(devicename)]
-#         hrs =device["off"]["hours"] if on_off==0 else device["on"]["hours"]
-#         mins =device["off"]["minutes"] if on_off==0 else device["on"]["minutes"]
-#         return (hrs, mins)
 
-# NOTE: Refer to the settings, we now have range of values for hours and minutes and hence the cron job is to fire  every minute - but we now block the action depending on if only there is a state change
-led_on_schedule = settings["crons"]["led"]["on"]
-led_off_schedule = settings["crons"]["led"]["off"]
-@sched.scheduled_job('cron', hour=led_on_schedule["hours"], minute=led_on_schedule["minutes"], timezone="Asia/Kolkata")
-def turn_on_led():
-    if hardware.relay_state(hardware.LEDGPIO)==0:
-        hardware.relay(hardware.LEDGPIO,1)
-        print("Turned on the LED now")
-@sched.scheduled_job('cron', hour=led_off_schedule["hours"], minute=led_off_schedule["minutes"], timezone="Asia/Kolkata")
-def turn_off_led():
-    if hardware.relay_state(hardware.LEDGPIO)==1:
-        hardware.relay(hardware.LEDGPIO,0)
-        print("Turned off the LED now")
+hardware.init()         # we are expecting the hardware GPIO to get initialized here
+@sched.scheduled_job('cron', hour=riseandshine["hours"], minute=riseandshine["minutes"], timezone="Asia/Kolkata")
+def rise_and_shine():
+    '''This gets triggered in the morning time , till midday
+    LED     :   ON
+    AIRPUMP :   ON
+    FILTER  :   OFF
+    FEEDER  :   OFF
+    life at the tank is encouraged to get in active state, woken up from the slumber
+    '''
+    # confirm shall return 0/-1 to confirm if the action was success
+    if  hardware.led_status()==0:
+        ok =hardware.turn_on_led()
+        if ok ==0:
+            logging.info("aqsm.schedules: The LED was turned ON")
+        else:
+            logging.warning("aqsm.schedules:rise_and_shine: Error turning the LED ON")
 
-# now working this for the airpump
-air_on_schedule = settings["crons"]["airpump"]["on"]
-air_off_schedule = settings["crons"]["airpump"]["off"]
-
-@sched.scheduled_job('cron', hour=air_on_schedule["hours"], minute=air_off_schedule["minutes"], timezone="Asia/Kolkata")
-def turn_on_air():
-    if hardware.relay_state(hardware.AIRGPIO)==0:
-        hardware.relay(hardware.AIRGPIO,1)
-        print("turned on the airpump")
-
-@sched.scheduled_job('cron', hour=air_off_schedule["hours"], minute=air_off_schedule["minutes"], timezone="Asia/Kolkata")
-def turn_off_air():
-    if hardware.relay_state(hardware.AIRGPIO)==1:
-        hardware.relay(hardware.AIRGPIO,0)
-        print("Turned off the airpump now")
-
-# now working this for the filter
-filter_on_schedule = settings["crons"]["filter"]["on"]
-filter_off_schedule = settings["crons"]["filter"]["off"]
-
-@sched.scheduled_job('cron', hour=filter_on_schedule["hours"], minute=filter_on_schedule["minutes"], timezone="Asia/Kolkata")
-def turn_on_filter():
-    if hardware.relay_state(hardware.FLTGPIO)==0:
-        hardware.relay(hardware.FLTGPIO,1)
-        print("turned on the filter")
-
-@sched.scheduled_job('cron', hour=filter_off_schedule["hours"], minute=filter_off_schedule["minutes"], timezone="Asia/Kolkata")
-def turn_off_filter():
-    if hardware.relay_state(hardware.FLTGPIO)==1:
-        hardware.relay(hardware.FLTGPIO,0)
-        print("Turned off the airpump now")
-
-# now working this for the feeder
-feeder_on_schedule = settings["crons"]["feeder"]["on"]
-feeder_off_schedule = settings["crons"]["feeder"]["off"]
-
-@sched.scheduled_job('cron', hour=feeder_on_schedule["hours"], minute=feeder_on_schedule["minutes"], timezone="Asia/Kolkata")
-def turn_on_feeder():
-    if hardware.relay_state(hardware.FEEDGPIO)==0:
-        hardware.relay(hardware.FEEDGPIO,1)
-        print("Turned on the feeder now")
-
-@sched.scheduled_job('cron', hour=feeder_off_schedule["hours"], minute=feeder_off_schedule["minutes"], timezone="Asia/Kolkata")
-def turn_off_feeder():
-    if hardware.relay_state(hardware.FEEDGPIO)==1:
-        hardware.relay(hardware.FEEDGPIO,0)
-        print("Turned off the feeder now")
+    confirm =hardware.turn_on_airpump()if hardware.airpump_status()==0 else None
+    if confirm !=None:
+        logging.info("aqsm.schedules: The airpump was turned ON")
+    else:
+        logging.debug("aqsm.schedules:The airpump was already ON")
+    confirm=hardware.turn_off_filter() if hardware.filter_status()==1 else None
+    if confirm !=None:
+        logging.info("aqsm.schedules: The filter was turned OFF")
+    else:
+        logging.debug("aqsm.schedules:The filter was already OFF")
+    confirm =hardware.turn_off_feeder() if hardware.feeder_status()==1 else None
+    if confirm !=None:
+        logging.info("aqsm.schedules: The feeder was turned OFF")
+    else:
+        logging.debug("aqsm.schedules:The feeder was already OFF")
---
+++ aqsm.device/schedules_archive.py
@@ -0,0 +1,98 @@
+#   project             :aquascape minder
+#   author              :kneerunjun@gmail.com
+#   development         :24-APRIL-2018
+#   purpose             :This helps setting up the cron jobs for the tank assiting tasks
+from apscheduler.schedulers.background import BackgroundScheduler
+import json, sys, datetime, pdb, hardware
+# initially we would want to pick from settings the cron job timing
+try:
+    with open('./settings.json') as data_file:
+        settings = json.load(data_file)
+        settings = settings["settings"]
+except FileNotFoundError as fe:
+    print("schedules.py : failed to open the settings.json file ,check if the file is present and valid")
+    sys.exit(1)
+
+# now once we have the settings , we need to go ahead and set initial state of the devices according to the time we have now. Else lets say we start the program at 18:01 , the jobs devices that should have been turned ON at 18:00 would not have started anyway
+# NOTE: Setting the initial state for each of the devices at the start of the program
+# everything here is based on finding the settings.json file
+if settings!=None:
+    sched = BackgroundScheduler()
+
+# def compress_list_tostring(lst):
+#     '''This helps to convert the incoming list from the json to a comma separated string
+#     Comma separated string is used to setup the cron job on specific hours
+#     '''
+#     return ','.join(map(str, lst))
+# def device_schedule(devicename, on_off=1):
+#     '''This digs into the settins dictionary and gets the ON, OFF schedule as required
+#     returns a tuple (hours , minutes) that signify the hours & minutes at which the triggers are fired. It spits out an list , so you may have to compress_list_tostring before setting up cron jobs
+#     on_off      : schedule for on time ==1 and schedule for off time ==0
+#     '''
+#     if settings!=None:
+#         device =settings["crons"]["{0}".format(devicename)]
+#         hrs =device["off"]["hours"] if on_off==0 else device["on"]["hours"]
+#         mins =device["off"]["minutes"] if on_off==0 else device["on"]["minutes"]
+#         return (hrs, mins)
+
+# NOTE: Refer to the settings, we now have range of values for hours and minutes and hence the cron job is to fire  every minute - but we now block the action depending on if only there is a state change
+led_on_schedule = settings["crons"]["led"]["on"]
+led_off_schedule = settings["crons"]["led"]["off"]
+@sched.scheduled_job('cron', hour=led_on_schedule["hours"], minute=led_on_schedule["minutes"], timezone="Asia/Kolkata")
+def turn_on_led():
+    if hardware.relay_state(hardware.LEDGPIO)==0:
+        hardware.relay(hardware.LEDGPIO,1)
+        print("Turned on the LED now")
+@sched.scheduled_job('cron', hour=led_off_schedule["hours"], minute=led_off_schedule["minutes"], timezone="Asia/Kolkata")
+def turn_off_led():
+    if hardware.relay_state(hardware.LEDGPIO)==1:
+        hardware.relay(hardware.LEDGPIO,0)
+        print("Turned off the LED now")
+
+# now working this for the airpump
+air_on_schedule = settings["crons"]["airpump"]["on"]
+air_off_schedule = settings["crons"]["airpump"]["off"]
+
+@sched.scheduled_job('cron', hour=air_on_schedule["hours"], minute=air_off_schedule["minutes"], timezone="Asia/Kolkata")
+def turn_on_air():
+    if hardware.relay_state(hardware.AIRGPIO)==0:
+        hardware.relay(hardware.AIRGPIO,1)
+        print("turned on the airpump")
+
+@sched.scheduled_job('cron', hour=air_off_schedule["hours"], minute=air_off_schedule["minutes"], timezone="Asia/Kolkata")
+def turn_off_air():
+    if hardware.relay_state(hardware.AIRGPIO)==1:
+        hardware.relay(hardware.AIRGPIO,0)
+        print("Turned off the airpump now")
+
+# now working this for the filter
+filter_on_schedule = settings["crons"]["filter"]["on"]
+filter_off_schedule = settings["crons"]["filter"]["off"]
+
+@sched.scheduled_job('cron', hour=filter_on_schedule["hours"], minute=filter_on_schedule["minutes"], timezone="Asia/Kolkata")
+def turn_on_filter():
+    if hardware.relay_state(hardware.FLTGPIO)==0:
+        hardware.relay(hardware.FLTGPIO,1)
+        print("turned on the filter")
+
+@sched.scheduled_job('cron', hour=filter_off_schedule["hours"], minute=filter_off_schedule["minutes"], timezone="Asia/Kolkata")
+def turn_off_filter():
+    if hardware.relay_state(hardware.FLTGPIO)==1:
+        hardware.relay(hardware.FLTGPIO,0)
+        print("Turned off the airpump now")
+
+# now working this for the feeder
+feeder_on_schedule = settings["crons"]["feeder"]["on"]
+feeder_off_schedule = settings["crons"]["feeder"]["off"]
+
+@sched.scheduled_job('cron', hour=feeder_on_schedule["hours"], minute=feeder_on_schedule["minutes"], timezone="Asia/Kolkata")
+def turn_on_feeder():
+    if hardware.relay_state(hardware.FEEDGPIO)==0:
+        hardware.relay(hardware.FEEDGPIO,1)
+        print("Turned on the feeder now")
+
+@sched.scheduled_job('cron', hour=feeder_off_schedule["hours"], minute=feeder_off_schedule["minutes"], timezone="Asia/Kolkata")
+def turn_off_feeder():
+    if hardware.relay_state(hardware.FEEDGPIO)==1:
+        hardware.relay(hardware.FEEDGPIO,0)
+        print("Turned off the feeder now")
