bcf31455cfdcdd1a8df6232df96b993a80cbf768
kneerunjun <kneerunjun@gmail.com>
2018-05-30 11:12:03 +0530

    resetting the branch code

--- aqsm.device/enable-service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo cp ./minder.service /etc/systemd/system/minder.service
-sudo systemctl enable minder.service
--- aqsm.device/remoteopen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ratom aqsm.log cloudlink.py hardware.py minder.py minder.upstart.py minder.restart.py schedules.py settings.json
